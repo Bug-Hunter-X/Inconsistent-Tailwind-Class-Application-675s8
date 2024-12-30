d3f65abb0524fccbe018659ed65748d431344496
BugHunter-X <askzenai@gmail.com>
2024-12-30 19:44:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// This is a simple component with Tailwind classes
+<div class="bg-gray-200 p-4">
+  <h1 class="text-3xl font-bold">Uncommon Tailwind Bug</h1>
+  <p class="mt-4">This component demonstrates an issue where Tailwind classes seem to be applied incorrectly, resulting in unexpected styles.</p>
+</div>
+```
